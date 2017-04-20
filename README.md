cdce946e62c7cf5cf828204af643e841d1c6b2d7
Max Matsumoto <mmmatsumoto@ucdavis.edu>
2017-04-20 13:28:47 -0700

    chore: update bootstrap.sh

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ boostrap.sh calls dotfiles.sh which sets up symlinks in the home directory. Then
 it runs through the set up of dein, the package manager I'm using for vim.
 Finally, it installs all the plugins and configures them.
 
+There may be additional set up (depends on system) to get YouCompleteMe running.
+Read the docs for what exactly is needed
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,19 @@ bash dotfiles.sh
 
 # install dein
 curl \
-  https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh\
+  https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh \
   | sh -s $HOME/.vim/bundle
 
 # update packages
 vim +dein#isntall +qall
 
 # configure plugins
-cd $HOME/.vim/bundle/repos/github.com/Valloric/YouCompleteMe\
-  && ./install.py --all
+cd $HOME/.vim/bundle/repos/github.com/Valloric/YouCompleteMe \
+  && ./install.py --clang-completer \
+                  # requires npm to be installed
+                  # --tern-completer \
+                  # requires go to be installed
+                  # --gocode-completer \
+                  # requires cargo to be installed
+                  # --racer-completer
 
